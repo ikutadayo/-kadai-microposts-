435c7ecd07ac4ebaa50eee1a16a7c2f298ad5218
sho.ikuta <sho.ikuta@rakuten.com>
2018-06-12 04:48:32 +0000

    post

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,19 +10,25 @@ class UsersController extends Controller
 {
     public function index()
     {
-        $users = User::paginate(10);
+        $users = User::paginate(1);
         
         return view('users.index', [
             'users' => $users,
         ]);
     }
     
-     public function show($id)
+      public function show($id)
     {
         $user = User::find($id);
+        $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
 
-        return view('users.show', [
+        $data = [
             'user' => $user,
-        ]);
+            'microposts' => $microposts,
+        ];
+
+        $data += $this->counts($user);
+
+        return view('users.show', $data);
     }
 }
